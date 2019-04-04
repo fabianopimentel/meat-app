3f7f46d8b406267c01a6285bcf0e98f9f242ba8c
Fabiano Pimentel <fabiano@pimentel.tech>
2019-04-04 00:16:35 -0300

    Menu item working dynamic

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -9,6 +9,7 @@ import { ErrorHandler } from "../app.error-handler"
 import { Restaurant } from "./restaurant/restaurant.model";
 
 import { MEAT_API } from "../app.api"
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
 
 @Injectable()
 export class RestaurantsService {
@@ -31,5 +32,11 @@ export class RestaurantsService {
     return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handleError)
   }
 }
